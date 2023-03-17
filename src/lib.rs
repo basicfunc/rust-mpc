68313cb9a4efd0b7de34ca5988c4cc44f206ce3c
Rahul <basicfunc@gmail.com>
2023-03-17 15:07:13 +0530

    Added mpc_input_t implementation and other related functions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,465 @@
 #![allow(non_camel_case_types)]
+use std::default;
 use std::fmt::Arguments;
 use std::fs::File;
+use std::io::{Read, Seek, SeekFrom, Write};
 use std::os::raw::*;
 
+#[derive(Debug, Clone, Copy, Default)]
 struct mpc_state_t {
-    pos: usize,
-    row: usize,
-    col: usize,
+    pos: i32,
+    row: i32,
+    col: i32,
     term: i32,
+}
+
+// State Types
+
+fn mpc_state_invalid() -> mpc_state_t {
+    mpc_state_t {
+        pos: -1,
+        row: -1,
+        col: -1,
+        term: 0,
+    }
+}
+
+fn mpc_state_new() -> mpc_state_t {
+    mpc_state_t {
+        pos: 0,
+        row: 0,
+        col: 0,
+        term: 0,
+    }
+}
+
+// Input Type
+const MPC_INPUT_STRING: usize = 0;
+const MPC_INPUT_FILE: usize = 1;
+const MPC_INPUT_PIPE: usize = 2;
+
+const MPC_INPUT_MARKS_MIN: usize = 32;
+
+const MPC_INPUT_MEM_NUM: usize = 512;
+
+struct mpc_mem_t {
+    mem: [char; 64],
+}
+
+struct mpc_input_t {
+    itype: usize,
+    filename: String,
+    state: mpc_state_t,
+
+    string: String,
+    buffer: Vec<char>,
+    file: Option<File>,
+
+    suppress: bool,
+    backtrack: bool,
+    marks_slot: i32,
+    marks_num: i32,
+    marks: Vec<mpc_state_t>,
+
+    lasts: Vec<char>,
+    last: char,
+
+    mem_index: usize,
+    mem_full: Vec<char>,
+    mem: Vec<mpc_mem_t>,
+}
+
+fn mpc_input_new_string(filename: &str, string: &str) -> mpc_input_t {
+    let itype = MPC_INPUT_STRING;
+    let filename = filename.to_owned();
+    let state = mpc_state_new();
+
+    let string = string.to_owned();
+    let buffer = vec![];
+    let file = None;
+
+    let suppress = false;
+    let backtrack = false;
+    let marks_num = 0;
+    let marks_slot = MPC_INPUT_MARKS_MIN as i32;
+    let marks: Vec<mpc_state_t> = Vec::with_capacity(marks_slot as usize);
+
+    let lasts: Vec<char> = Vec::with_capacity(marks_slot as usize);
+    let last = '\0';
+
+    let mem_index = 0;
+    let mem_full: Vec<char> = vec!['\0'; MPC_INPUT_MEM_NUM];
+    let mem = Vec::with_capacity(MPC_INPUT_MEM_NUM);
+
+    mpc_input_t {
+        itype,
+        filename,
+        state,
+        string,
+        buffer,
+        file,
+        suppress,
+        backtrack,
+        marks_slot,
+        marks_num,
+        marks,
+        lasts,
+        last,
+        mem_index,
+        mem_full,
+        mem,
+    }
+}
+
+fn mpc_input_new_nstring(filename: &str, string: &str, length: usize) -> mpc_input_t {
+    let itype = MPC_INPUT_STRING;
+    let filename = filename.to_owned();
+    let state = mpc_state_new();
+
+    let string = string[..length].to_owned();
+    let buffer = vec![];
+    let file = None;
+
+    let suppress = false;
+    let backtrack = false;
+    let marks_num = 0;
+    let marks_slot = MPC_INPUT_MARKS_MIN as i32;
+    let marks: Vec<mpc_state_t> = Vec::with_capacity(marks_slot as usize);
+
+    let lasts: Vec<char> = Vec::with_capacity(marks_slot as usize);
+    let last = '\0';
+
+    let mem_index = 0;
+    let mem_full: Vec<char> = vec!['\0'; MPC_INPUT_MEM_NUM];
+    let mem = Vec::with_capacity(MPC_INPUT_MEM_NUM);
+
+    mpc_input_t {
+        itype,
+        filename,
+        state,
+        string,
+        buffer,
+        file,
+        suppress,
+        backtrack,
+        marks_slot,
+        marks_num,
+        marks,
+        lasts,
+        last,
+        mem_index,
+        mem_full,
+        mem,
+    }
+}
+
+fn mpc_input_new_pipe(filename: &str, pipe: File) -> mpc_input_t {
+    let itype = MPC_INPUT_PIPE;
+    let filename = filename.to_owned();
+    let state = mpc_state_new();
+
+    let string = String::new();
+    let buffer = vec![];
+    let file: Option<File> = Some(pipe);
+
+    let suppress = false;
+    let backtrack = false;
+    let marks_num = 0;
+    let marks_slot = MPC_INPUT_MARKS_MIN as i32;
+    let marks: Vec<mpc_state_t> = Vec::with_capacity(marks_slot as usize);
+
+    let lasts: Vec<char> = Vec::with_capacity(marks_slot as usize);
+    let last = '\0';
+
+    let mem_index = 0;
+    let mem_full: Vec<char> = vec!['\0'; MPC_INPUT_MEM_NUM];
+    let mem = Vec::with_capacity(MPC_INPUT_MEM_NUM);
+
+    mpc_input_t {
+        itype,
+        filename,
+        state,
+        string,
+        buffer,
+        file,
+        suppress,
+        backtrack,
+        marks_slot,
+        marks_num,
+        marks,
+        lasts,
+        last,
+        mem_index,
+        mem_full,
+        mem,
+    }
+}
+
+fn mpc_input_new_file(filename: &str, file: File) -> mpc_input_t {
+    let itype = MPC_INPUT_STRING;
+    let filename = filename.to_owned();
+    let state = mpc_state_new();
+
+    let string = String::new();
+    let buffer = vec![];
+    let file = Some(file);
+
+    let suppress = false;
+    let backtrack = false;
+    let marks_num = 0;
+    let marks_slot = MPC_INPUT_MARKS_MIN as i32;
+    let marks: Vec<mpc_state_t> = Vec::with_capacity(marks_slot as usize);
+
+    let lasts: Vec<char> = Vec::with_capacity(marks_slot as usize);
+    let last = '\0';
+
+    let mem_index = 0;
+    let mem_full: Vec<char> = vec!['\0'; MPC_INPUT_MEM_NUM as usize];
+    let mem = Vec::with_capacity(MPC_INPUT_MEM_NUM as usize);
+
+    mpc_input_t {
+        itype,
+        filename,
+        state,
+        string,
+        buffer,
+        file,
+        suppress,
+        backtrack,
+        marks_slot,
+        marks_num,
+        marks,
+        lasts,
+        last,
+        mem_index,
+        mem_full,
+        mem,
+    }
+}
+
+fn mpc_input_delete(i: &mut mpc_input_t) {
+    drop(i)
+}
+
+fn is_ptr_inside<T>(p: *const T, q: &Vec<T>) -> bool {
+    let q_start = q.as_ptr() as usize;
+    let q_end = q_start + q.len() * std::mem::size_of::<T>();
+    let p_addr = p as usize;
+    p_addr >= q_start && p_addr < q_end
+}
+
+// TODO: Implemented Naively, more work needed.
+fn mpc_mem_ptr(i: &mpc_input_t, p: *const u8) -> bool {
+    let start = i.mem.as_ptr() as *const u8;
+    let end = unsafe { start.add(MPC_INPUT_MEM_NUM * std::mem::size_of::<mpc_mem_t>()) };
+    p >= start && p < end
+}
+
+// mpc_malloc, mpc_calloc, mpc_realloc and mpc_free are skipped as they don't need to implemented because Rust handle them gracefully.
+
+// TODO: Implemented Naively, more work needed.
+fn mpc_export(i: &mut mpc_input_t, p: *const u8) {
+    if mpc_mem_ptr(i, p) {
+        return;
+    }
+    unimplemented!()
+}
+
+fn mpc_input_backtrack_disable(i: &mut mpc_input_t) {
+    i.backtrack = false;
+}
+
+fn mpc_input_backtrack_enable(i: &mut mpc_input_t) {
+    i.backtrack = true;
+}
+
+fn mpc_input_suppress_disable(i: &mut mpc_input_t) {
+    i.suppress = false;
+}
+
+fn mpc_input_suppress_enable(i: &mut mpc_input_t) {
+    i.suppress = true;
+}
+
+fn mpc_input_mark(i: &mut mpc_input_t) {
+    if !i.backtrack {
+        return;
+    }
+
+    i.marks_num += 1;
+
+    if i.marks_num > i.marks_slot {
+        i.marks_slot = i.marks_num + i.marks_num / 2;
+        i.marks.resize(i.marks_slot as usize, Default::default());
+        i.lasts.resize(i.marks_slot as usize, Default::default());
+    }
+
+    i.marks[(i.marks_num - 1) as usize] = i.state;
+    i.lasts[(i.marks_num - 1) as usize] = i.last;
+
+    if i.itype == MPC_INPUT_PIPE && i.marks_num == 1 {
+        i.buffer.resize(1, Default::default())
+    }
+}
+
+fn mpc_input_unmark(i: &mut mpc_input_t) {
+    if !i.backtrack {
+        return;
+    }
+
+    if i.marks_slot > i.marks_num + i.marks_num / 2 && i.marks_slot > MPC_INPUT_MARKS_MIN as i32 {
+        i.marks_slot = if i.marks_num > MPC_INPUT_MARKS_MIN as i32 {
+            i.marks_num
+        } else {
+            MPC_INPUT_MARKS_MIN as i32
+        }
+    }
+
+    if i.itype == MPC_INPUT_PIPE && i.marks_num == 0 {
+        if i.file.is_none() {
+            panic!("Error: No File Opened.")
+        } else {
+            let f = i.file.as_mut().unwrap();
+            for idx in (0..i.buffer.len()).rev() {
+                match write!(f, "{}", i.buffer[idx]) {
+                    Ok(_) => (),
+                    Err(e) => panic!("Error: {e} occured."),
+                }
+            }
+
+            i.buffer.clear();
+        }
+    }
+}
+
+fn mpc_input_rewind(i: &mut mpc_input_t) {
+    if !i.backtrack {
+        return;
+    }
+
+    i.state = i.marks[(i.marks_num - 1) as usize];
+    i.last = i.lasts[(i.marks_num - 1) as usize];
+
+    if i.itype == MPC_INPUT_FILE {
+        i.file
+            .as_ref()
+            .unwrap()
+            .seek(SeekFrom::Start(i.state.pos as u64))
+            .unwrap();
+    }
+
+    mpc_input_mark(i);
+}
+
+fn mpc_input_buffer_in_range(i: &mpc_input_t) -> bool {
+    i.state.pos < (i.buffer.len() as i32 + i.marks[0].pos)
+}
+
+fn mpc_input_buffer_get(i: &mpc_input_t) -> char {
+    i.buffer[(i.state.pos - i.marks[0].pos) as usize]
+}
+
+fn mpc_input_getc(i: &mpc_input_t) -> char {
+    if i.itype == MPC_INPUT_STRING {
+        return i.string.chars().nth(i.state.pos as usize).unwrap();
+    } else if i.itype == MPC_INPUT_FILE {
+        let mut buf = [0u8; 1];
+        i.file.as_ref().unwrap().read(&mut buf).unwrap();
+        return buf[0] as char;
+    } else if i.itype == MPC_INPUT_PIPE {
+        if i.buffer.len() > 0 && mpc_input_buffer_in_range(i) {
+            return mpc_input_buffer_get(i);
+        } else {
+            let mut buf = [0u8; 1];
+            i.file.as_ref().unwrap().read(&mut buf).unwrap();
+            return buf[0] as char;
+        }
+    } else {
+        return '\0';
+    }
+}
+
+fn mpc_input_peekc(i: &mpc_input_t) -> char {
+    if i.itype == MPC_INPUT_STRING {
+        return i.string.chars().nth(i.state.pos as usize).unwrap();
+    } else if i.itype == MPC_INPUT_FILE {
+        let mut buf = [0u8; 1];
+        let bytes_read = i.file.as_ref().unwrap().read(&mut buf).unwrap();
+
+        if bytes_read == 0 {
+            return '\0';
+        }
+
+        i.file
+            .as_ref()
+            .unwrap()
+            .seek(SeekFrom::Current(-1))
+            .unwrap();
+
+        return buf[0] as char;
+    } else if i.itype == MPC_INPUT_PIPE {
+        if i.buffer.len() > 0 && mpc_input_buffer_in_range(i) {
+            return mpc_input_buffer_get(i);
+        } else {
+            let mut buf = [0u8; 1];
+            let bytes_read = i.file.as_ref().unwrap().read(&mut buf).unwrap();
+
+            if bytes_read == 0 {
+                return '\0';
+            }
+            let c = buf[0] as char;
+            write!(i.file.as_ref().unwrap(), "{c}").unwrap();
+            return c;
+        }
+    } else {
+        return '\0';
+    }
+}
+
+fn mpc_input_terminated(i: &mpc_input_t) -> bool {
+    mpc_input_peekc(i) == '\0'
+}
+
+fn mpc_input_failure(i: &mpc_input_t, ch: char) {
+    match i.itype {
+        MPC_INPUT_FILE => {
+            i.file
+                .as_ref()
+                .unwrap()
+                .seek(SeekFrom::Current(-1))
+                .unwrap();
+        }
+        MPC_INPUT_PIPE => {
+            if i.buffer.is_empty() || (i.buffer.len() > 0 && !mpc_input_buffer_in_range(i)) {
+                write!(i.file.as_ref().unwrap(), "{ch}").unwrap();
+            }
+        }
+        _ => (),
+    }
+}
+
+fn mpc_input_success(i: &mut mpc_input_t, c: char, o: Vec<&str>) {
+    if i.itype == MPC_INPUT_PIPE && !i.buffer.is_empty() && !mpc_input_buffer_in_range(i) {
+        let s = i.buffer.len();
+        i.buffer.resize(s + 2, Default::default());
+        let s = i.buffer.len();
+        i.buffer[s + 1] = '\0';
+        i.buffer[s + 0] = c;
+    }
+
+    i.last = c;
+    i.state.pos += 1;
+    i.state.col += 1;
+
+    if c == '\n' {
+        i.state.col = 0;
+        i.state.row += 1;
+    }
+
+    if !o.is_empty() {
+        // TODO
+        unimplemented!()
+    }
 }
 
 // Error Type
